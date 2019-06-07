a52842eae1e97a7f2b4f8fe2afbe29b48bb48cce
Peder Larson <peder.larson@ucsf.edu>
2019-06-06 20:01:19 -0700

    Create utebrain_t1_signal_model.m

---
+++ Fitting/utebrain_t1_signal_model.m
@@ -0,0 +1,11 @@
+    function Smodel = utebrain_t1_signal_model(x, num_components, TE, flip, TR)
+	num_variables = 5;
+	% x contains M0, T2, df, phi, T1
+        Smodel = 0;
+        for n = 0:num_components-1
+            Smodel = Smodel + x(num_variables*n+1) * sin(flip) * (1-exp(-TR/x(num_variables*n+5))/(1-cos(flip)*exp(-TR/x(num_variables*n+5)) *  ...
+		exp( -TE(:)/x(num_variables*n+2)-i*2*pi*x(num_variables*n+3)*TE(:) + i*x(num_variables*n+4));
+        end            
+        
+    end
+
