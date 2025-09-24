ca8c4a55820fc209e4e56c8641c853455612809c
Bechtel <aryil.bechtel@ucsf.edu>
2025-09-24 11:40:39 -0700

    remove hard coding of trajectory parameters in ute rosette

--- ImageReconstruction/generate_dual_echo_rosette_trajectory.m
+++ ImageReconstruction/generate_dual_echo_rosette_trajectory.m
@@ -16,43 +16,69 @@ function [trajectory_te1, trajectory_te2] = generate_dual_echo_rosette_trajector
     % Calculate kmax
     kmax = matrix_size / fov / 2;
 
-    % Startup/wind-down weighting
-    weighted = linspace(0,0.95,44);
-    weighted_cum = cumsum(weighted);
+    % Set trajectory parameters
+    npoints_petal = 432; % number of points per petal
+    npoints_zero_start = 2; % number of zero points in beginning of each petal
+    linear_spacing = pi/390; % spacing of points in linear section of each petal
+    npoints_ramp = 40; % number of points in non linear ramp up/down of each petal
+    npoints_alpha = 190; % number of points to sample alpha angle
+    npoints_phi = 378; % number of points to sample phi angle
+    ramp_weights = cumsum(linspace(0,1,npoints_ramp)); % defines the scaling of how trajectory is ramped up/down
+    
+    % Calculate number of points in linear section of each petal
+    npoints_linear = npoints_petal - (npoints_ramp*2 + npoints_zero_start);
+    
+    % Calculate index for start of each region of petal
+    idx_ramp_up_start = npoints_zero_start + 1;
+    idx_linear_start = idx_ramp_up_start + npoints_ramp;
+    idx_ramp_down_start = idx_linear_start + npoints_linear;
     
     % Initialize k-space arrays
-    kx = zeros(190,378,432);
-    ky = zeros(190,378,432);
-    kz = zeros(190,378,432);
+    kx = zeros(npoints_alpha,npoints_phi,npoints_petal);
+    ky = zeros(npoints_alpha,npoints_phi,npoints_petal);
+    kz = zeros(npoints_alpha,npoints_phi,npoints_petal);
     
     % Build trajectory
-    for ll = 1:190
-        alpha = pi/189 * (ll - 1);
+    for ll = 1:npoints_alpha
+        alpha = pi/(npoints_alpha-1) * (ll - 1);
     
-        for kk = 1:378
-            phi = 2*pi/378 * (kk - 1);
+        for kk = 1:npoints_phi
+            phi = 2*pi/npoints_phi * (kk - 1);
     
-            % Startup region
-            for jj = 1:44
-                w = weighted_cum(jj);
-                ang = pi/(195*2) * w;
+            % Initialize index counters for ramp weight array
+            pp = 1;
+            qq = 1;
+    
+            % Ramp-up region
+            for jj = idx_ramp_up_start:idx_ramp_up_start + npoints_ramp - 1
+                % w = weighted_cum(jj);
+                weight = ramp_weights(pp);
+                pp = pp + 1;
+                ang = linear_spacing * weight;
                 kx(ll,kk,jj) = kmax*sin(ang)*cos(ang+phi)*cos(-pi/2+alpha);
                 ky(ll,kk,jj) = kmax*sin(ang)*sin(ang+phi)*cos(-pi/2+alpha);
                 kz(ll,kk,jj) = kmax*sin(ang)*sin(-pi/2+alpha);
             end
     
-            % Main region
-            for jj = 45:392
-                ang = pi/(195*2) * (jj-23);
+            % Linear region
+            for jj = idx_linear_start:idx_linear_start + npoints_linear - 1
+                weight = weight + 1;
+                ang = linear_spacing * weight;
                 kx(ll,kk,jj) = kmax*sin(ang)*cos(ang+phi)*cos(-pi/2+alpha);
                 ky(ll,kk,jj) = kmax*sin(ang)*sin(ang+phi)*cos(-pi/2+alpha);
                 kz(ll,kk,jj) = kmax*sin(ang)*sin(-pi/2+alpha);
             end
     
-            % Wind-down region
-            for jj = 393:432
-                w = (195*2) - weighted_cum(437-jj);
-                ang = pi/(195*2) * w;
+            % Ramp-down region
+            for jj = idx_ramp_down_start:idx_ramp_down_start + npoints_ramp - 1
+                if qq < length(ramp_weights)
+                    weight = pi/linear_spacing - ramp_weights(end - qq);
+                else
+                    % repeat last point on trajectory
+                    weight = pi/linear_spacing - ramp_weights(1);
+                end
+                qq = qq + 1;
+                ang = pi/(195*2) * weight;
                 kx(ll,kk,jj) = kmax*sin(ang)*cos(ang+phi)*cos(-pi/2+alpha);
                 ky(ll,kk,jj) = kmax*sin(ang)*sin(ang+phi)*cos(-pi/2+alpha);
                 kz(ll,kk,jj) = kmax*sin(ang)*sin(-pi/2+alpha);
@@ -64,17 +90,17 @@ function [trajectory_te1, trajectory_te2] = generate_dual_echo_rosette_trajector
     kx = permute(kx,[3 2 1]);
     ky = permute(ky,[3 2 1]);
     kz = permute(kz,[3 2 1]);
-
-    % Normalize to max = pi
+    
+     % Normalize to max = pi
     kx = pi / max(abs(kx(:))) * kx;
     ky = pi / max(abs(ky(:))) * ky;
     kz = pi / max(abs(kz(:))) * kz;
-
+    
     % Extract first and second echo trajectories
     kx_te1 = kx(2:217,:,:);
     ky_te1 = ky(2:217,:,:);
     kz_te1 = kz(2:217,:,:);
-
+    
     kx_te2 = kx(216:431,:,:);
     ky_te2 = ky(216:431,:,:);
     kz_te2 = kz(216:431,:,:);
