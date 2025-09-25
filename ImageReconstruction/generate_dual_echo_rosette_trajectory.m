6fc26b0c89d90d25c750bfc66fa64f9fc2d9bc42
Bechtel <aryil.bechtel@ucsf.edu>
2025-09-25 09:55:19 -0700

    experiment with different weights

--- ImageReconstruction/generate_dual_echo_rosette_trajectory.m
+++ ImageReconstruction/generate_dual_echo_rosette_trajectory.m
@@ -22,7 +22,15 @@ function [trajectory_te1, trajectory_te2] = generate_dual_echo_rosette_trajector
     npoints_ramp = 40; % number of points in non linear ramp up/down of each petal
     npoints_alpha = 190; % number of points to sample alpha angle
     npoints_phi = 378; % number of points to sample phi angle
-    ramp_weights = cumsum(linspace(0,1,npoints_ramp)); % defines the scaling of how trajectory is ramped up/down
+
+    % Set weights to define scalinig of how trajectory is ramped up/down
+    preweights_short = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, ...
+        0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95];
+    preweights = zeros(1, 2*length(preweights_short));
+    preweights(1:2:end) = preweights_short;
+    preweights(2:2:end) = preweights_short;
+    ramp_weights = cumsum(preweights);
+    % ramp_weights = cumsum(linspace(0.05,0.95,npoints_ramp));  
     
     % Set starting point and length of first and second echo along a petal
     % (setting a fraction will incrementally shift the trajectory)
